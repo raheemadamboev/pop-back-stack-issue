58b3eb142e87ab991c56d707083518ecc5744828
Raheem <raheemideadev@gmail.com>
2024-06-15 02:28:02 +0500

    Setup Material3;

--- app/src/main/java/xyz/teamgravity/popbackstackissue/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/popbackstackissue/MainActivity.kt
@@ -5,24 +5,22 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import xyz.teamgravity.popbackstackissue.ui.theme.PopBackStackIssueTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             PopBackStackIssueTheme {
-                Scaffold(
+                Surface(
+                    color = MaterialTheme.colorScheme.background,
                     modifier = Modifier.fillMaxSize()
-                ) { innerPadding ->
-
+                ) {
                 }
             }
         }
