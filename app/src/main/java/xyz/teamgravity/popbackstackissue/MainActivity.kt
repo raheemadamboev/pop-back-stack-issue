2035f5ffbcafa2a7d8315ed2127642212158dec5
Raheem <raheemideadev@gmail.com>
2024-06-15 02:50:36 +0500

    Fixed navigating back issue;

--- app/src/main/java/xyz/teamgravity/popbackstackissue/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/popbackstackissue/MainActivity.kt
@@ -40,9 +40,7 @@ class MainActivity : ComponentActivity() {
 
                         composable("screen_b") {
                             ScreenB(
-                                onBack = {
-                                    controller.popBackStack()
-                                }
+                                onBack = controller::navigateUp
                             )
                         }
                     }
